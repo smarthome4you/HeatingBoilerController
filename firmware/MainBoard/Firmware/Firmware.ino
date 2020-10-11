62bc03bb4968c5c2dc75ac35121aaa95621279a2
Krzysztof Kaczkowski <grafvader@gmail.com>
2020-10-12 00:37:47 +0200

    Test

--- firmware/MainBoard/Firmware/Firmware.ino
+++ firmware/MainBoard/Firmware/Firmware.ino
@@ -1,52 +1,136 @@
 #include <Wire.h>
 #include <math.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-const int hallPin = 8; //sygnal - czarny, niebieski - GND, brązowy - VCC
-int hallState = 0;
 
-int pompaCO=2; 
-int pompaCWU=3;
-int pompaPDL=4;
-int podajnik=5;
-int nadmuch=10;
-int tempKotla=11;
-int tempCWU=12;
-int tempSpalin=13;
-int tempUjscia=14;
+/*
+ * Firmware dla kotła z podajnikiem szufladkowym
+ * Podajnik zawsze musi wykonać pełny obrót!
+ */
 
-int state [][3] = {
-                   {1,2,3},
-                   {2,3,42},
-                   {2,2,3}
-                   };
+#define pinPompaCO     A2 
+#define pinPompaCWU    A3
+#define pinPompaPDL    A4
+#define pinPodajnik    A5
+#define pinNadmuch     A10
+#define pinTempKotla_1 A12
+#define pinTempKotla_2 A13
+#define pinTempCWU     A14
+#define pinHallotron   A16
 
+#define czujnikTempKotla_1 0
+#define czujnikTempKotla_2 1
+#define czujnikTempCO      2
+#define czujnikTempCWU     3
+
+#define okresPomiaruTemp  1000 // co jaki czas maja byc odczytywane czujniki temperatury 1000 = 1 sec
+
+#define ostatniPomiarTemp  0
+#define ostatniaZmianaTemp 1
+#define ostatniaTemp       2
+
+#define deltaTemp          1 // 1 stopień celcjusza
+
+float tempZadanaKociol = 55;
+float tempZadanaCWU    = 55;
+float tempHistereza    = 5;
+int   przepalanie      = 40*60; // w sekundach 40 minut
+
+int temperatura [][3]  = {
+                           {0, 0, 0},   // Temp kotła punkt pomiaru 1 (czas ostatniego pomiaru, czas ostatniej zmiany temperatury, temp ostatniego pomiaru)
+                           {0, 0, 0},   // Temp kotła punkt pomiaru 2 (czas ostatniego pomiaru, czas ostatniej zmiany temperatury, temp ostatniego pomiaru)
+                           {0, 0, 0},   // Temp CWU (czas ostatniego pomiaru, czas ostatniej zmiany temperatury, temp ostatniego pomiaru)
+                         };
+
+int dmuchawa [][1]     = {
+                           {1}     // czas ostaniego załaczenia
+                         };
+
+
+int przekaznik [][1]   = {
+                           {1},   // czas ostaniego załączenia pompa CO
+                           {1},   // czas ostaniego załączenia pompa CWU
+                           {1},   // czas ostaniego załączenia pompa podłoga
+                           {1}    // czas ostaniego załączenia podajnika
+                         };
+
+
+OneWire wireTempKotla_1(pinTempKotla_1); 
+OneWire wireTempKotla_2(pinTempKotla_2); 
+OneWire wireTempCWU(pinTempCWU);  
+DallasTemperature sensorTempKotla_1(&wireTempKotla_1);
+DallasTemperature sensorTempKotla_2(&wireTempKotla_2);
+DallasTemperature sensorTempCWU(&wireTempCWU);
 
 void WireInit() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  //Wire.begin(8); 
   Serial.print("START\n");
 }
 
-double Thermister(int RawADC) {  //Function to perform the fancy math of the Steinhart-Hart equation
- double Temp;
- Temp = log(((10240000/RawADC) - 10000));
- Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
- Temp = Temp - 273.15;              // Convert Kelvin to Celsius
- Temp = (Temp * 9.0)/ 5.0 + 32.0; // Celsius to Fahrenheit - comment out this line if you need Celsius
- return Temp;
+
+float AktualizujTempKotla_1() {
+  /* 
+   * Nie odczytujemy czujnika za czesto bo podnosi to jego temperature i zakłamuje wynik robimy to raz na 1 sek.  
+   */
+  
+  bool newMeasurement = false;
+  
+  if (temperatura[czujnikTempKotla_1][ostatniPomiarTemp] == 0) {
+    temperatura[czujnikTempKotla_1][ostatniPomiarTemp]  = millis(); 
+    temperatura[czujnikTempKotla_1][ostatniaZmianaTemp] = millis();
+    newMeasurement = true;
+  }
+
+  // Czy upłynął wystarczający czas od ostatniego pomiaru aby wykonać pomiar ponownie
+  if (  abs(millis() - temperatura[czujnikTempKotla_1][ostatniPomiarTemp]) > okresPomiaruTemp) {
+    newMeasurement = true;
+  }
+  
+  if ( newMeasurement ) {
+     sensorTempKotla_1.begin();
+     sensorTempKotla_1.requestTemperatures();   
+     float tempKotla = sensorTempKotla_1.getTempCByIndex(0);
+
+     // Kiedy ostatnio zmieniła się temperatura o deltaTemp
+     if ( abs(temperatura[czujnikTempKotla_1][ostatniaTemp] - tempKotla) > deltaTemp ) {
+          temperatura[czujnikTempKotla_1][ostatniaZmianaTemp] = millis();
+     }
+     temperatura[czujnikTempKotla_1][ostatniaTemp] = tempKotla;
+     temperatura[czujnikTempKotla_1][ostatniPomiarTemp] = millis(); 
+  }
+  
+  return temperatura[czujnikTempKotla_1][ostatniaTemp];
 }
+
+float AktualizujTempKotla_2() {
+  // Nie odczytujemy czujnika za czesto bo podnosi to jego temperature i zakłamuje wynik 
+  // robimy to raz na 1 sek.
+  return 0;
+}
+
+float AktualizujTempCWU() {
+  // Nie odczytujemy czujnika za czesto bo podnosi to jego temperature i zakłamuje wynik 
+  // robimy to raz na 1 sek.
+  return 0;
+}
+
+bool CheckList() {
+  return true;
+}
+
 
 void setup(){
   WireInit();
-  analogReference(DEFAULT);
-  pinMode(A7, INPUT_PULLUP);
+  
 
 /*
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   pinMode(relay3, OUTPUT);
   pinMode(relay4, OUTPUT);
+  analogReference(DEFAULT);
+  pinMode(A7, INPUT_PULLUP);
   */
 }
 
@@ -70,9 +154,8 @@ void test_relay() {
 */
 int val;
 void loop(){
- 
-  val=analogRead(A7);      //Read the analog port 0 and store the value in val
-  //temp=Thermister(val);   //Runs the fancy math on the raw analog value
-  Serial.println(val); 
-  delay(500);
+    AktualizujTempKotla_1();
+    AktualizujTempKotla_2();
+    AktualizujTempCWU();
+
 }
